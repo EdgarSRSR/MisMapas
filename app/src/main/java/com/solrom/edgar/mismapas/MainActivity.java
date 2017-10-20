9bfcc194189ad3b81bd0437aff41d0d292d02e5d
edgarsr <edgarsolisromeu@gmail.com>
2017-10-20 00:27:51 -0500

    Initial commit

---
+++ app/src/main/java/com/solrom/edgar/mismapas/MainActivity.java
@@ -0,0 +1,51 @@
+package com.solrom.edgar.mismapas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*public void irMapa(View v){
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
+    }*/
+
+
+    public void mostrarMapa(View v){
+        int ImageView = v.getId();
+
+        switch (ImageView){
+            case R.id.naucalli:
+                crearIntentMapa(MapsActivity.class, 19.49161800380091, -99.24013511684575, "Parque Naucalli");
+                        break;
+            case R.id.alamedaNorte:
+                crearIntentMapa(MapsActivity.class, 19.500204536424565, -99.17833702114262, "Parque Alameda Norte");
+                break;
+            case R.id.bicentenario:
+                crearIntentMapa(MapsActivity.class, 19.470124391556602, -99.19113652256169, "Parque Bicentenario");
+                break;
+            case R.id.plansexenal:
+                crearIntentMapa(MapsActivity.class, 19.454495481070534, -99.17284385708012, "Deportivo Plan Sexenal");
+                break;
+        }
+    }
+
+    public void crearIntentMapa(Class<MapsActivity> mapsActivityClass, Double latitud, Double longitud, String lugar){
+        Intent intentMapa = new Intent(this, mapsActivityClass);
+        intentMapa.putExtra("latitud", latitud);
+        intentMapa.putExtra("longitud", longitud);
+        intentMapa.putExtra("lugar", lugar);
+        startActivity(intentMapa);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/solrom/edgar/mismapas/MapsActivity.java
@@ -0,0 +1,103 @@
+package com.solrom.edgar.mismapas;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
+
+    private GoogleMap mMap;
+    private Double latitud;
+    private Double longitud;
+    private String lugar;
+    private Button btnregresar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        btnregresar = (Button) findViewById(R.id.btnRegresar);
+        btnregresar.setOnClickListener(this);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null)
+        {
+            latitud = extras.getDouble("latitud");
+            if(latitud == null){
+                Toast.makeText(this, "No se encuentra la latitud.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            longitud = extras.getDouble("longitud");
+            if(longitud == null){
+                Toast.makeText(this, "No se encuentra la longitud.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            lugar = extras.getString("lugar");
+            if(lugar == null){
+                Toast.makeText(this, "No shay descripción de este lugar.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+        }
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng parque = new LatLng(latitud, longitud);
+        mMap.addMarker(new MarkerOptions().position(parque).title(lugar).icon(BitmapDescriptorFactory.fromResource(R.drawable.icons8_terraria_48)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(parque));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitud, longitud),16.0f));
+    }
+
+    public void regresar(View v){
+
+        switch (v.getId()){
+            case R.id.btnRegresar:
+                Intent intent = new Intent(MapsActivity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.equals(btnregresar)){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
+    }
+}
